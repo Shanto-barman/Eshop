f108637ae14d0ba8dfcca79774379d0606add6e7
Shanta Barman <sha681nta@gmail.com>
2025-11-07 05:06:31 +0600

    app updat

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,43 +1,60 @@
-import React from 'react'
-import { Button } from './components/ui/button'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Navbar from './components/ui/Navbar';
-import  Home  from './pages/Home';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import Verify from './pages/Verify';
-import VerifyEmail from './pages/VerifyEmail';
-
-
-
-
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Verify from "./pages/Verify";
+import VerifyEmail from "./pages/VerifyEmail";
 
 const router = createBrowserRouter([
-   {
-    path:'/Home',
-    element :<><Navbar/><Home/></>
+  {
+    path: "/",
+    element: (
+      <>
+        <Navbar />
+        <Home />
+      </>
+    ),
   },
   {
-    path:'/Signup',
-    element:<><Signup/></>
+    path: "/Signup",
+    element: (
+      <>
+        <Signup />
+      </>
+    ),
   },
-    {
-    path:'/login',
-    element:<><Login/></>
+  {
+    path: "/login",
+    element: (
+      <>
+        <Login />
+      </>
+    ),
   },
-     {
-    path:'/Verify',
-    element:<><Verify/></>
+  {
+    path: "/Verify",
+    element: (
+      <>
+        <Verify />
+      </>
+    ),
   },
-    {
-    path:'/Verify/:token',
-    element:<><VerifyEmail/></>
+  {
+    path: "/Verify/:token",
+    element: (
+      <>
+        <VerifyEmail />
+      </>
+    ),
   },
-  ])
+]);
 
-  const App = ()=>{
-   return( <>
-      <RouterProvider router = {router}/>
-    </>)
-  }
-export default App
+const App = () => {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+};
+export default App;
